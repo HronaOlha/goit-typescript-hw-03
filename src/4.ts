3a1f21f8638b3fd9a6790ea303abf717ea9ae1cd
Грона Оля <grona.hell@gmail.com>
2024-03-02 11:31:14 +0200

    done all tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,40 @@
+class Key {
+    private readonly signature = Math.random();
+
+    getSignature() {
+        return this.signature
+    }
+}
+
+class Person {
+    constructor(private key: Key) { }
+    getKey() {
+        return this.key
+    }
+}
+
+abstract class House {
+    protected door = false;
+    protected key: Key;
+    protected tennants: Person[]= [];
+
+        constructor(key: Key) {
+        this.key = key
+    }
+
+    comeIn(person: Person) {
+        if(this.door && !this.tennants.includes(person)){this.tennants.push(person)}
+    }
+
+    abstract openDoor(key: Key):void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key) {
+        if (key.getSignature() === this.key.getSignature()){this.door = true}
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
